45070d080ac16fd148667ceb8b487bf505086a69
Ethan Simmons <simmonsde@s.dcsdk12.org>
2015-09-18 10:13:45 -0600

    Tests for middles

--- arrays_test.rb
+++ arrays_test.rb
@@ -11,4 +11,9 @@ class ArraysTest < MiniTest::Test
    	end.new
 	end
 
+	def test_middles
+		assert_equal [2, 3], @m.middles([1, 2, 3], [2, 3, 4])
+		assert_equal [0, 1], @m.middles([0, 0, 0], [2, 1, 2])
+		assert_equal [-5, 4], @m.middles([10, -5, 1], [2, 4, 5])
+	end
 end
